9fa14f482795bc893b6dc58b703e71bc0d623f6e
Tony Rasa <trasa@meancat.com>
2025-01-02 13:19:15 -0800

    add arrow form

--- 5.1-helloWorldButCleaner.js
+++ 5.1-helloWorldButCleaner.js
@@ -4,18 +4,25 @@ const app = express();
 const port = 4242;
 
 // anonymous function that has 3 named args
+// uses "Arrow" Syntax instead of "function" keyword
 app.use((req, res, next) => {
     console.log("Middleware Handler Here");
     next();
 });
 
-// another anonymous function
+// another anonymous function, Arrow vs. Function
 app.get('/', (req, res) => {
     console.log(`Received request for '${req.url}' from agent ${req.headers['user-agent']}`);
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/plain");
     res.end("Hello World\n");
 });
+
+// an anonymous function that calls itself immediately?!
+((msg) => {
+    console.log(`Hello me, ${msg}`);
+})("myself, and I!");
+
 
 // one more anonymous function
 app.listen(port, () => { console.log(`started server at ${port}`); });
